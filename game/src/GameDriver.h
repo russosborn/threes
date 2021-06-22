f5afd9aaa2519c778862accf99dd9c889fc4a49f
russellosborn <russellosborn@penguin>
2021-06-21 21:22:28 -0500

    implement all the GameDriver fns (still needs tests)

--- game/src/GameDriver.h
+++ game/src/GameDriver.h
@@ -47,9 +47,6 @@ namespace threes {
     private:
       BoardPtr m_boardPtr;
       CardSequencePtr m_cardSeqPtr;
-
-      
-
       
     }; // class GameDriver
 
@@ -66,9 +63,112 @@ namespace threes {
 	}
 	m_boardPtr = std::make_unique( initialCards );
       }
+
+    
+
+    template<class BOARD>
+    void GameDriver<BOARD>::render() const {
+      static constexpr unsigned CardValuePrintWidth = 6;
+      
+      for(unsigned row = 0; row < BOARD::dim; ++row) {
+	for(unsigned col = 0; col < BOARD::dim; ++col) {
+	  std::cout << std::setw(CardValuePrintWidth) <<
+	    m_boardPtr->cardAtIndex( row, col ).value << std::endl;
+	}
+	std::cout << std::endl << std::endl;
+      }
+      std::cout << "Next: " << std::setw(CardValuePrintWidth) <<
+	m_cardSeqPtr->peek(m_boardPtr).value;
+      
     }
     
+    template<class BOARD>
+    MoveResult GameDriver<BOARD>::move(const ShiftDirection dir) {
+      // if shift is valid, then apply it
+      if( m_boardPtr->canShift(dir) ) {
+	Card toInsert = m_cardSeqPtr->draw()
+	  m_boardPtr->shiftBoard(dir, toInsert);
+      } else {
+	return MOVE_INVALID;
+      }
+
+      // check to see if any new move can happen
+      bool anyRemainingMoves = false;
+      for( auto dir : {DIRECTION_UP, DIRECTION_DOWN, DIRECTION_LEFT, DIRECTION_RIGHT} ) {
+	if( m_boardPtr->canShift(dir) ) {
+	  return MOVE_VALID;
+	}
+      }
+      // if no further moves were possible, game should end
+      return END_GAME;
+    }
+
+
+    template<class BOARD>
+    uint64_t GameDriver<BOARD>::gameScore() {
+      // todo: should score counting logic be in the Card class or the
+      // GameDriver class? For now, put it here so other GameDriver impls
+      // can choose their own method.
+
+      auto boardData = m_boardPtr->underlyingDataRef();
+      uint64_t result;
+      for( auto cardData : boardData ) {
+	// 1 and 2 don't contribute to score
+	if( cardData.value > 2) { result += cardData.value; }
+      }
+      return result;
+    }
     
-  
+
+    template<class BOARD>
+    uint64_t GameDriver<BOARD>::play() {
+      // using w/a/s/d for now because arrow keys are apparently
+      // a platform-specific nightmare. Could possible solve with ncurses
+      // or a real GUI
+
+      char moveChar = 'x';
+      MoveResult lastMove = MOVE_VALID;
+      while( lastMove != END_GAME ) {
+
+	if( lastMove == MOVE_INVALID) {
+	  std::cout << "You tried move " << moveChar
+		    << " but that move is illegal."
+		    << "Try a different move. " << std::endl;
+	}
+	
+	std::cin >> moveChar;
+
+	bool validInput = true;
+	ShiftDirection moveDir;
+	switch(moveChar) {
+	case 'w':
+	  moveDir = DIRECTION_UP;
+	  break;
+	case 'a':
+	  moveDir = DIRECTION_LEFT;
+	  break;
+	case 's':
+	  moveDir = DIRECTION_DOWN;
+	  break;
+	case 'd':
+	  moveDir = DIRECTION_RIGHT;
+	  break;
+	default:
+	  validInput = false;
+	  break;
+	}
+
+	if( !validInput ) {
+	  std::cout << "HEY! Input one of w/a/s/d for up/left/down/right!"
+	}
+	else {
+	  lastMove = move(moveDir);
+	}
+      }
+
+      std::cout << "No more valid moves! Game over, your score is "
+		<< gameScore() << std::endl;
+    }
+    
   } // ns game 
 } // ns threes
